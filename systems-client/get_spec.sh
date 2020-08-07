05da581dd3dc0da3b87ef7a35661ea3504b6a459
Steve Black <scblack@tacc.utexas.edu>
2020-08-07 10:17:53 -0500

    Systems: Update get_spec.sh to use current branch name

--- systems-client/get_spec.sh
+++ systems-client/get_spec.sh
@@ -9,7 +9,15 @@ cd $RUN_DIR
 
 # Create target dir in case not yet created by maven
 mkdir -p $PRG_PATH/target
+
+# Figure out which branch we are on so we can find the correct spec file
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+# If branch name is HEAD or empty as might be the case in a jenkins job then
+#   set it to GIT_BRANCH. Jenkins jobs should have this set in the env.
+if [ -z "$BRANCH_NAME" -o "x$BRANCH_NAME" = "xHEAD" ]; then
+  BRANCH_NAME=$(echo "$GIT_BRANCH" | awk -F"/" '{print $2}')
+fi
 # Download latest openapi spec from repo
-# TODO: generalize for dev, staging and prod branches
-# Dev yaml
-curl -o target/SystemsAPI.yaml https://raw.githubusercontent.com/tapis-project/tapis-java/dev/tapis-systemsapi/src/main/resources/SystemsAPI.yaml
+SPEC_FILE=https://raw.githubusercontent.com/tapis-project/tapis-java/${BRANCH_NAME}/tapis-systemsapi/src/main/resources/SystemsAPI.yaml
+echo "Downloading openapi spec file: ${SPEC_FILE}"
+curl -o target/SystemsAPI.yaml ${SPEC_FILE}
