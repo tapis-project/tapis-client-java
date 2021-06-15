5896669bbe401a44b81e5483e9f70806d48770a2
Smruti Padhy <spadhy@tacc.utexas.edu>
2021-06-15 17:02:46 -0500

    GetTokenParms fixed

--- auth-client/src/main/java/edu/utexas/tacc/tapis/auth/client/model/GetTokenParms.java
+++ auth-client/src/main/java/edu/utexas/tacc/tapis/auth/client/model/GetTokenParms.java
@@ -1,9 +1,9 @@
 package edu.utexas.tacc.tapis.auth.client.model;
 
-import edu.utexas.tacc.tapis.auth.client.gen.model.InlineObject1;
+import edu.utexas.tacc.tapis.auth.client.gen.model.InlineObject2;
 
 /** Wrapper class for the generated create token model class.
  * 
  * @author rcardone
  */
-public class GetTokenParms extends InlineObject1 { }
+public class GetTokenParms extends InlineObject2 { }
