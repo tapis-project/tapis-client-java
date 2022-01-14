e266e8c54a0559e0154b6a5a5361a5ba902d14eb
Steve Black <scblack@tacc.utexas.edu>
2022-01-14 12:51:30 -0600

    Authenticator: Fix issue with build after openapi spec change.

--- auth-client/src/main/java/edu/utexas/tacc/tapis/auth/client/model/GetTokenParms.java
+++ auth-client/src/main/java/edu/utexas/tacc/tapis/auth/client/model/GetTokenParms.java
@@ -1,9 +1,10 @@
 package edu.utexas.tacc.tapis.auth.client.model;
 
 import edu.utexas.tacc.tapis.auth.client.gen.model.InlineObject2;
+import edu.utexas.tacc.tapis.auth.client.gen.model.InlineObject3;
 
 /** Wrapper class for the generated create token model class.
  * 
  * @author rcardone
  */
-public class GetTokenParms extends InlineObject2 { }
+public class GetTokenParms extends InlineObject3 { }
